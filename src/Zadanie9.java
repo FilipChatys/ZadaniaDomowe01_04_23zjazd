fb0ed10b103383ce3bb87cdaeb52f36ea71b3fb8
fchatys <fchatys@gmail.com>
2023-04-15 12:46:47 +0200

    Drobne zmiany

--- src/Zadanie9.java
+++ src/Zadanie9.java
@@ -3,12 +3,16 @@
 // znajdujące się w tej tablicy. Należy wykorzystać sito Eratostenesa. Sito Eratostenesa znajdziecie na wikipedii.
 public class Zadanie9 {
     public static void main(String[] args) {
+        System.out.println("Tablica przed operacją");
         for (int i = 2; i <= 101; i++) {
-                System.out.print(isPrime(i) + " ");
+                System.out.print(i + " ");
             }
+        System.out.println("\n");
+        System.out.println("Tablica liczb pierwszych");
+        for (int j = 2; j<=101; j++){
+            System.out.print(isPrime(j) + " ");
         }
-
-
+    }
     public static String isPrime(int n) {
         if (n <= 1) {
             return "";
